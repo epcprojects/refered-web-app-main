779b5905f600940755d7c8c4e26c3e2f62b245df
Hamza Shah <hamzagi345@gmail.com>
2025-02-14 01:05:00 +0500

    feat: User can able to add their own custom business name while signing up.

--- src/containers/common/select-business-type-options.tsx
+++ src/containers/common/select-business-type-options.tsx
@@ -1,16 +1,23 @@
 'use client';
 
 import EmptyList from '@/components/common/empty-list';
+import { Form } from '@/components/form';
+import FieldButton from '@/components/form/field-button';
+import FieldInput from '@/components/form/field-input';
 import AuthCardLayout from '@/components/layout/auth-card-layout';
 import { Input } from '@/components/ui/input';
 import { Spinner } from '@/components/ui/spinner';
 import { GetAllBusinessTypes, IBusinessType } from '@/firebase/business-types';
 import { cn } from '@/utils/cn.utils';
 import { asyncGuard } from '@/utils/lodash.utils';
+import { generateUUID } from '@/utils/misc.utils';
 import { useDebounceValue } from '@/utils/use-hooks.utils';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { RiCheckboxCircleFill, RiSearchLine } from 'react-icons/ri';
 import { toast } from 'sonner';
+import { z } from 'zod';
 
 interface IProps {
   handleGoBack: () => void;
@@ -19,11 +26,30 @@ interface IProps {
   notForAuthPage?: boolean;
 }
 
+export type businessOptionsSchemaType = z.infer<typeof businessOptionsSchema>;
+export const businessOptionsSchema = z.object({
+  category: z.string(),
+});
+
 const SelectBusinessTypeOptions: React.FC<IProps> = ({ handleSelectOption, selectedOption, handleGoBack, notForAuthPage }) => {
   const [isFetching, setIsFetching] = useState(false);
   const [allOptions, setAllOptions] = useState<IBusinessType[]>([]);
   const [filteredOptions, setFilteredOptions] = useState<IBusinessType[]>([]);
   const [debouncedSearchTerm, setSearchTerm] = useDebounceValue('', 500);
+
+  const form = useForm<businessOptionsSchemaType>({ resolver: zodResolver(businessOptionsSchema) });
+
+  const onSubmit = async (values: businessOptionsSchemaType) => {
+    if (!values.category) return;
+
+    const userAddedBusiness: IBusinessType = {
+      id: generateUUID(),
+      name: values.category,
+      description: '',
+    };
+
+    setFilteredOptions((prev) => [...prev, userAddedBusiness]);
+  };
 
   const handleFetchData = async () => {
     setIsFetching(true);
@@ -67,6 +93,24 @@ const SelectBusinessTypeOptions: React.FC<IProps> = ({ handleSelectOption, selec
               ))}
             </>
           )}
+
+          {/* Other */}
+          <Form form={form} onSubmit={onSubmit} className="grid w-full gap-2.5">
+            <div className="relative border-solid">
+              <FieldInput form={form} name="category" placeholder="Enter Category" className="w-[calc(100%-60px)] border-gray-300" containerClassName="h-[50px] bg-transparent border-gray-300" />
+              <FieldButton
+                form={form}
+                type="submit"
+                label="Add"
+                variant="secondary"
+                style={{
+                  position: 'absolute',
+                  top: 7,
+                  right: 7,
+                }}
+              />
+            </div>
+          </Form>
         </>
       )}
     </AuthCardLayout>
