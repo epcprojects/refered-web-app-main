d01b51985300364a7d8d16ad3642cbce3ac5c596
Hamza Shah <hamzagi345@gmail.com>
2025-03-02 16:17:16 +0500

    fix: UI fixes.
    fix: Show redeem buttons when referred by is me.

--- src/containers/profile/profile-referrals-list.tsx
+++ src/containers/profile/profile-referrals-list.tsx
@@ -130,7 +130,8 @@ export const MutualFavourites: React.FC<IMutualFavourites> = ({ mutualFavourites
 export const ReferralUserChip: React.FC<{ id: string; src?: string; name: string; shade?: 'dark' | 'light'; className?: string; showAvatar?: boolean }> = ({ id, src, name, shade = 'light', showAvatar = true, className = 'rounded-full' }) => (
   <NextLink href={`${AppPages.PROFILE}/${id}`} className={`flex cursor-pointer items-center gap-1 border-1 ${shade === 'dark' ? 'border-black border-opacity-15' : 'border-border'} p-[3px] transition-all hover:bg-slate-100 ${className}`}>
     {showAvatar === true && <Avatar src={src} alt="Profile Picture" fallback={initials('Mohsin').slice(0, 1)} className="!h-4 !w-4" fallbackClassName="!text-[9px]" />}
-    <span className="mr-0.5 overflow-hidden text-ellipsis whitespace-nowrap text-[11px]">{name}</span>
+
+    <span className="mr-0.5 w-max max-w-[150px] overflow-hidden text-ellipsis whitespace-nowrap text-[11px] sm:max-w-sm md:max-w-[150px]">{name}</span>
   </NextLink>
 );
 
@@ -172,38 +173,33 @@ const ReferralItem: React.FC<IProps & { data: IReferral; isRedeemed: boolean }> 
         <NextLink href={`${AppPages.PROFILE}/${userReferral?.UserId}`} className="cursor-pointer">
           <Avatar src={type === 'business' ? data.referredToUser?.ImageUrl : data.referredBusinessUser?.ImageUrl} alt={[data.referredBusinessUser?.FirstName, data.referredBusinessUser?.LastName].join(' ').trim()} fallback={initials([data.referredBusinessUser?.FirstName, data.referredBusinessUser?.LastName].join(' ').trim()).slice(0, 2)} />
         </NextLink>
-        <div className="w-full">
-          <div className="flex flex-row gap-3">
+        <div className="w-full overflow-hidden">
+          <div className="flex flex-row gap-1">
             <div className="flex w-full max-w-full flex-1 flex-col overflow-hidden">
               <NextLink href={`${AppPages.PROFILE}/${userReferral?.UserId}`} className="cursor-pointer">
                 <h3 className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-normal">{(userReferral?.BusinessName || userReferral?.FirstName || 'New Referral').trim()}</h3>
               </NextLink>
-              <p className="space-x-1 overflow-hidden text-ellipsis whitespace-nowrap text-xs font-normal text-muted-foreground">
-                {userReferral?.City && userReferral?.State && (
-                  <div className="mb-1 mt-[5px] flex gap-1 text-muted-foreground">
-                    <RiMapPin2Line size={15} />
-                    <p className="my-auto w-[97%] space-x-1 overflow-hidden text-ellipsis whitespace-nowrap text-xs font-normal text-muted-foreground">
-                      <span>{userReferral.City + ', ' + userReferral.State}</span>
-                    </p>
-                  </div>
-                )}
-
-                {(userReferral?.BusinessTypeName || userReferral?.FirstName || userReferral?.LastName) && (
-                  <div className="!ml-0 flex gap-1 text-muted-foreground">
-                    <RiNewsLine size={15} />
-                    <p className="my-auto w-[97%] space-x-1 overflow-hidden text-ellipsis whitespace-nowrap text-xs font-normal text-muted-foreground">
-                      <span>{userReferral?.BusinessTypeName}</span>
-                      <span className="mt-1">•</span>
-                      <span>{[userReferral?.FirstName, userReferral?.LastName].join(' ').trim()}</span>
-                    </p>
-                  </div>
-                )}
-              </p>
+              {userReferral?.City && userReferral?.State && (
+                <div className="mb-1 mt-[5px] flex gap-1 text-muted-foreground">
+                  <RiMapPin2Line size={15} />
+                  <p className="my-auto w-[97%] overflow-hidden text-ellipsis whitespace-nowrap text-xs font-normal text-muted-foreground">{userReferral.City + ', ' + userReferral.State}</p>
+                </div>
+              )}
+              {(userReferral?.BusinessTypeName || userReferral?.FirstName || userReferral?.LastName) && (
+                <div className="!ml-0 flex gap-1 text-muted-foreground">
+                  <RiNewsLine size={15} />
+                  <p className="my-auto w-[97%] overflow-hidden text-ellipsis whitespace-nowrap text-xs font-normal text-muted-foreground">
+                    <span>{userReferral?.BusinessTypeName}</span>
+                    <span className="mt-1"> • </span>
+                    <span>{[userReferral?.FirstName, userReferral?.LastName].join(' ').trim()}</span>
+                  </p>
+                </div>
+              )}
             </div>
             <div className="flex flex-shrink-0 flex-col items-end gap-0.5">
               <p className="text-xs text-muted-foreground">{date.format(date.fromUnixTime(data.datetime.seconds), 'dd MMM yyyy')}</p>
-              <div className="flex flex-row gap-1">
-                {hasRedeemed || (type === 'to' && myType === 'to') ? (
+              <div className="flex flex-row flex-wrap gap-1">
+                {hasRedeemed || (type === 'to' && myType === 'to') || (type === 'by' && myType === 'by') ? (
                   <Button variant="secondary" size="sm" classes={{ container: 'disabled:bg-foreground/5 disabled:text-foreground rounded-md p-[5px] px-2.5 h-max' }} disabled={hasRedeemed || isRedeeming || isInitiatingChat} onClick={() => setIsRedeemPopupOpened(true)}>
                     {isRedeeming ? <Spinner color="secondary" size="sm" /> : hasRedeemed ? 'Redeemed' : 'Redeem'}
                   </Button>
@@ -235,7 +231,7 @@ const ReferralItem: React.FC<IProps & { data: IReferral; isRedeemed: boolean }> 
             </div>
           </div>
           {/* {type === 'business' ? ( */}
-          <div className="mt-3 flex flex-col items-start gap-1.5 2xs:flex-row 2xs:items-center">
+          <div className="mt-3 flex flex-wrap items-center gap-1.5">
             <ReferralUserChip id={data.referredByUser?.UserId || ''} className="rounded-sm" src={data.referredByUser?.ImageUrl} name={[data.referredByUser?.FirstName].join(' ').trim()} />
             <span className="text-xs text-muted-foreground">refer'd to</span>
             <ReferralUserChip id={data.referredToUser?.UserId || ''} className="rounded-sm" src={data.referredToUser?.ImageUrl} name={[data.referredToUser?.FirstName].join(' ').trim()} />
