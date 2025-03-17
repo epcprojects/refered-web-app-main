ac63dde3a80a338620bb9b62b8cb574421c4f18e
Hamza Shah <hamzagi345@gmail.com>
2025-03-17 23:23:09 +0500

    fix: Fixed regex for special characters.
    chore: Added missing cities for U.S countries.

--- src/constants/app-regex.constants.ts
+++ src/constants/app-regex.constants.ts
@@ -3,6 +3,6 @@ export const AppRegex = {
   ALPHANUMERIC: new RegExp(/^[a-zA-Z0-9]+$/),
   ATLEAST_ONE_LOWERCASE_LETTER: new RegExp(/(?=.*[a-z])/),
   ATLEAST_ONE_UPPERCASE_LETTER: new RegExp(/(?=.*[A-Z])/),
-  ATLEAST_ONE_SPECIAL_CHARACTER: new RegExp(/(?=.*[!@#$%^&*(),.?":{}|<>])/),
+  ATLEAST_ONE_SPECIAL_CHARACTER: new RegExp(/(?=.*[!@#$%^&*(),.?'":{}|<>_\-+=~`;_\\/\[\]<>*&^%$#@!])/),
   NO_CODE_ALLOWED: new RegExp(/<[^>]*>|<\/[^>]*>|<script.*?>.*?<\/script>|style=["'][^"']*["']/i),
 };
